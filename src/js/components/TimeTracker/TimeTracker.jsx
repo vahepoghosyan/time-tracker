42da0abed66520a95a96bdf237553ca85800d54c
Vahe Poghosyan <vahe.poghosyan.js@gmail.com>
2018-10-24 23:42:37 +0400

    reimplementing time tracker

--- src/js/components/TimeTracker/TimeTracker.jsx
+++ src/js/components/TimeTracker/TimeTracker.jsx
@@ -1,141 +1,136 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import PropTypes from 'prop-types';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import './TimeTracker.scss';
-import { createMuiTheme } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
-
-function TabContainer(props) {
-    return (
-        <Typography component="div" style={{ padding: 8 * 3 }}>
-            {props.children}
-        </Typography>
-    );
-}
 
 const styles = (theme) => ({
     button: {
         margin: theme.spacing.unit,
     },
-    palette: {
-        primary: blue,
+    input: {
+        display: 'none',
     },
 });
 
-TabContainer.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-
-
 class TimeTracker extends React.Component {
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+    };
+
     state = {
         value: 0,
-        timer: {
-            hour: '00',
-            minute: '00',
-            second: '00',
-        },
+        timer: 0,
+        timeSummary: 0,
         isStarted: false,
         trackerDailyHistory: [],
     };
 
-        if (!this.state.isStarted) {
+    summaryCounter = (timer) => {
+        const hour = parseInt(timer / 3600);
+
+        const minute = parseInt(timer / 60) - hour * 60;
+
+        const seconds = parseInt(timer) - hour * 3600 - minute * 60;
+
+        return `${hour.toString().padStart(2, '0')} : 
+                ${minute.toString().padStart(2, '0')} : 
+                ${seconds.toString().padStart(2, '0')}`;
+    };
+
+    getDate = (time) => {
+        const date = new Date();
+        let currentTime = '';
+        let currentDate = '';
+
+        if (time) {
+            currentTime = `${date.getHours().toString().padStart(2, '0')} :
+                           ${date.getMinutes().toString().padStart(2, '0')} :
+                           ${date.getSeconds().toString().padStart(2, '0')}`;
+        } else {
+            currentDate = `${date.getDate().toString().padStart(2, '0')} : 
+                           ${date.getMonth().toString().padStart(2, '0')} : 
+                           ${date.getFullYear().toString().padStart(2, '0')}`;
+        }
+
+        return currentTime || currentDate;
+    };
+
     handleStartTracker = () => {
+        const { timeSummary, isStarted } = this.state;
+
+        if (!isStarted) {
             const date = new Date();
-            let second = 1;
-            let minute = 0;
-            let hour = 0;
-            const currentSecond = date.getSeconds().toString().padStart(2, '0');
-            const currentMinute = date.getMinutes().toString().padStart(2, '0');
-            const currentHour = date.getHours().toString().padStart(2, '0');
-            const currentTime = `${currentHour} : ${currentMinute} : ${currentSecond}`;
-            const dailyDeltaTime = [];
+            const getTimeTrackerDailyData = JSON.parse(localStorage.getItem('TimeTrackerDailyData'));
+            const totalSummarySecondFromStorage = getTimeTrackerDailyData ?
+                getTimeTrackerDailyData.map((item) => (date.getDate().toString() === item.date.slice(0, 2) ?
+                    item.delta : 0))
+                    .reduce((accumulator, currentValue) => accumulator + currentValue) : 0;
 
-            this.setState({
-                timer: {
-                    hour: '00',
-                    minute: '00',
-                    second: `0${second}`,
+            let timerSecond = 0;
+            let totalSummarySecond = totalSummarySecondFromStorage || timeSummary;
+
+            const startTime = this.getDate(true);
+
+            const currentDate = this.getDate();
+
+            const temporaryValue = [
+                {
+                    temporary: {
+                        startTime,
+                    },
+                    date: currentDate,
                 },
-            });
+            ];
 
-            dailyDeltaTime.push({
-                startTime: currentTime,
-            });
+            if (getTimeTrackerDailyData) {
+                getTimeTrackerDailyData.push(temporaryValue[0]);
+                localStorage.setItem('TimeTrackerDailyData', JSON.stringify(getTimeTrackerDailyData));
+            } else {
+                localStorage.setItem('TimeTrackerDailyData', JSON.stringify(temporaryValue));
+            }
 
             this.intervalState = setInterval(() => {
-                second++;
+                timerSecond += 1;
+                totalSummarySecond += 1;
 
-                if (second === 60) {
-                    minute++;
-                    second = '00';
-                }
-                if (minute === 60) {
-                    hour++;
-                    minute = '00';
-                }
+                const endTime = this.getDate(true);
 
-                if (String(second).length !== 2) {
-                    second = `0${String(second)}`;
-                }
-
-                if (String(minute).length !== 2) {
-                    minute = `0${String(minute)}`;
-                }
-
-                if (String(hour).length !== 2) {
-                    hour = `0${String(hour)}`;
+                if (getTimeTrackerDailyData) {
+                    getTimeTrackerDailyData[getTimeTrackerDailyData.length - 1].temporary.endTime = endTime;
+                    getTimeTrackerDailyData[getTimeTrackerDailyData.length - 1].delta = timerSecond;
+                    localStorage.setItem('TimeTrackerDailyData', JSON.stringify(getTimeTrackerDailyData));
                 }
 
                 this.setState({
-                    timer: {
-                        hour,
-                        minute,
-                        second,
-                    },
+                    timer: timerSecond,
+                    timeSummary: totalSummarySecond,
                     isStarted: true,
                 });
-
-                const dailyDeltaTimeToString = JSON.stringify(dailyDeltaTime);
-
-                localStorage.setItem('TimeTrackerData', dailyDeltaTimeToString);
-            }, 100);
+            }, 1000);
         }
     };
 
-        if (this.state.isStarted) {
-            const date = new Date();
-            const timeTrackerData = JSON.parse(localStorage.getItem('TimeTrackerData'));
-            const currentSecond = date.getSeconds().toString().length !== 2 ? `0${date.getSeconds().toString()}` : date.getSeconds().toString();
-            const currentMinute = date.getMinutes().toString().length !== 2 ? `0${date.getMinutes().toString()}` : date.getMinutes().toString();
-            const currentHour = date.getHours().toString().length !== 2 ? `0${date.getHours().toString()}` : date.getHours().toString();
-
-            const currentTime = `${currentHour} : ${currentMinute} : ${currentSecond}`;
     handleEndTracker = () => {
+        const { timer, isStarted, trackerDailyHistory } = this.state;
 
-            const currentTotalDate = `${this.state.timer.hour} : ${this.state.timer.minute} : ${this.state.timer.second}`;
-            const trackerDailyHistory = [...this.state.trackerDailyHistory];
+        if (isStarted) {
+            const endTime = this.getDate(true);
 
-            trackerDailyHistory.push(currentTotalDate);
+            const getTimeTrackerDailyData = JSON.parse(localStorage.getItem('TimeTrackerDailyData'));
 
-            timeTrackerData[timeTrackerData.length - 1].endTime = currentTime;
+            getTimeTrackerDailyData[getTimeTrackerDailyData.length - 1].temporary.endTime = endTime;
+            getTimeTrackerDailyData[getTimeTrackerDailyData.length - 1].delta = timer;
+            localStorage.setItem('TimeTrackerDailyData', JSON.stringify(getTimeTrackerDailyData));
 
-            timeTrackerData[timeTrackerData.length - 1].trackTime = `${this.state.timer.hour} : ${this.state.timer.minute} : ${this.state.timer.second}`;
+            trackerDailyHistory.push(this.summaryCounter(timer));
 
-            localStorage.setItem('TimeTrackerData', JSON.stringify(timeTrackerData));
             clearInterval(this.intervalState);
-
             this.setState({
-                timer: {
-                    hour: '00',
-                    minute: '00',
-                    second: '00',
-                },
+                timer: 0,
                 isStarted: false,
                 trackerDailyHistory,
             });
@@ -146,31 +141,45 @@ class TimeTracker extends React.Component {
         this.setState({ value });
     };
 
+    componentWillMount() {
+        const getTimeTrackerDailyData = JSON.parse(localStorage.getItem('TimeTrackerDailyData'));
+
+        const date = new Date();
+
+        const totalSummarySecondFromStorage = getTimeTrackerDailyData ?
+            getTimeTrackerDailyData.map((item) => (date.getDate().toString() === item.date.slice(0, 2) ? item.delta : 0))
+                .reduce((accumulator, currentValue) => accumulator + currentValue) : 0;
+
+        const trackerDailyHistory = getTimeTrackerDailyData ?
+            getTimeTrackerDailyData.map((item) => (date.getDate().toString() === item.date.slice(0, 2) ?
+                this.summaryCounter(item.delta) : '')) : [];
+
+        const currentTime = date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds();
+
+        const updateInterval = 86400 - currentTime;
+
+        this.dailyInterval = setInterval(() => {
+            this.setState([]);
+        }, updateInterval * 1000);
+
+        this.setState({
+            timeSummary: totalSummarySecondFromStorage,
+            trackerDailyHistory,
+        });
+    }
+
     render() {
         const { classes } = this.props;
-        const { value } = this.state;
+        const {
+            value,
+            timer,
+            timeSummary,
+            trackerDailyHistory,
+        } = this.state;
 
-        const timer = `${this.state.timer.hour} : ${this.state.timer.minute} : ${this.state.timer.second}`;
-        const trackerDailyHistory = [...this.state.trackerDailyHistory];
-        const trackerDailyHistoryItems = trackerDailyHistory.map((item, key) => <li key={key}>{item}</li>);
-
-        let totalHour = 0;
-        let totalMinute = 0;
-        let totalSecond = 0;
-        let totalTime = '';
-
-        for (let i = 0, len = trackerDailyHistory.length; i < len; i++) {
-            totalHour += Number(trackerDailyHistory[i].slice(0, 2));
-            totalMinute += Number(trackerDailyHistory[i].slice(5, 7));
-            totalSecond += Number(trackerDailyHistory[i].slice(10, 12));
-        }
-
-        totalHour = totalHour.toString().length === 2 ? totalHour : `0${totalHour.toString()}`;
-        totalMinute = totalMinute.toString().length === 2 ? totalMinute : `0${totalMinute.toString()}`;
-        totalSecond = totalSecond.toString().length === 2 ? totalSecond : `0${totalSecond.toString()}`;
-        if (totalSecond.toString() !== '00') {
-            totalTime = `${totalHour} : ${totalMinute} : ${totalSecond}`;
-        }
+        const trackerDailyHistoryItems = trackerDailyHistory.map((item) => item && <li key={Math.random()}>{item}</li>);
+        const currentDelta = this.summaryCounter(timer) || '00 : 00 : 00';
+        const totalSummary = this.summaryCounter(timeSummary);
 
         return (
             <div className="wrapper">
@@ -180,9 +189,8 @@ class TimeTracker extends React.Component {
                     </Tabs>
                 </AppBar>
                 {value === 0 && (
-                <TabContainer>
                     <div className="Container">
-                        <div className="Timer">{timer}</div>
+                        <div className="Timer">{currentDelta}</div>
                         <div>
                             <Button
                                 variant="contained"
@@ -201,17 +209,12 @@ class TimeTracker extends React.Component {
                             </Button>
                         </div>
                         <ul className="trackerDailyHistory">{trackerDailyHistoryItems}</ul>
-                        <div className="Total">{totalTime}</div>
+                        <div className="Total">Total: {totalSummary}</div>
                     </div>
-                </TabContainer>
                 )}
             </div>
         );
     }
 }
-
-TimeTracker.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(TimeTracker);
